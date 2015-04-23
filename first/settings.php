0d4cd705743df5ed446f58167b7ca30f3fab5d46
EldinSalkovic <eldinsalkoviic@gmail.com>
2015-04-23 15:39:52 +0200

    First commit

---
+++ first/settings.php
@@ -0,0 +1,42 @@
+<?php
+	$adresa[0]="1300 W Sunset Rd Henderson,NV  89014 0000"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="GALLERIA SUNSET ";
+	$telephone[0]=7024361558;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="4300 Meadows Lane Ste 273 Las Vegas,NV  89107 0000"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="MEADOWS MALL ";
+	$telephone[1]=7028781936;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="4500 N Oracle Rd Tucson,AZ  85705 0000"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="TUCSON MALL ";
+	$telephone[2]=5202937373;// Broj koji se poziva na dugme CALL US
+
+	$adresa[3]="1365 North Dupont Hwy Dover,DE  19901 0000"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[3]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[3]="DOVER MALL ";
+	$telephone[3]=3027344841;// Broj koji se poziva na dugme CALL US
+
+	$adresa[4]="4737 Concord Pike Wilmington,DE  19803 0000"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[4]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[4]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[4]="CONCORD MALL";
+	$telephone[4]=3024785141;// Broj koji se poziva na dugme CALL US
+
+	$adresa[5]="101 Rangeline Rd Joplin,MO  64801 0000"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[5]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[5]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[5]="NORTHPARK MALL ";
+	$telephone[5]=4177824101;// Broj koji se poziva na dugme CALL US
+
+	$centratelephone=3052522000;
+	$companynumber=6;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
